12452bed70e076da5fdcb4c5f620a71ebdf6d32c
Alexander Akimov <akimov.infotech@gmail.com>
2019-07-12 23:54:14 +0600

    Users page

---
+++ VOD.Admin/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VOD.Database.Services;
+using VOD.Domain.DTOModles.Admin;
+
+namespace VOD.Admin.Pages.Users
+{
+    [Authorize(Roles ="Admin")]
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<UserDTO> Users = new List<UserDTO>();
+        [TempData]
+        public string Alert { get; set; }
+
+        private readonly IUserService _userService;
+        public IndexModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public async Task OnGetAsync()
+        {
+            Users = await _userService.GetUsersAsync();
+        }
+    }
+}
