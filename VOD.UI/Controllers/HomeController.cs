0b8739ffd167e0881f5ce80eb8c9431a88942a17
Alexander Akimov <akimov.infotech@gmail.com>
2019-07-21 01:14:08 +0600

    test queries

--- VOD.UI/Controllers/HomeController.cs
+++ VOD.UI/Controllers/HomeController.cs
@@ -14,19 +14,29 @@ namespace VOD.UI.Controllers
     public class HomeController : Controller
     {
         private IUIReadService _uiReadService;
+        private IDbReadService _dbReadService;
         private SignInManager<VODUser> _signInManager;
-        public HomeController(SignInManager<VODUser> signInMgr, IUIReadService uiReadService)
+        public HomeController(SignInManager<VODUser> signInMgr, 
+            IUIReadService uiReadService,
+            IDbReadService dbReadService)
         {
             _signInManager = signInMgr;
             _uiReadService = uiReadService;
+            _dbReadService = dbReadService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             // var courses = await _uiReadService.GetCoursesAsync("25c705c0-76c8-4296-89d2-2128deb96280");
             // var course = await _uiReadService.GetCourseAsync("25c705c0-76c8-4296-89d2-2128deb96280", 1);
             // var video = await _uiReadService.GetVideoAsync("25c705c0-76c8-4296-89d2-2128deb96280", 1);
             // var videos = await _uiReadService.GetVideosAsync("25c705c0-76c8-4296-89d2-2128deb96280", 1);
+
+            //var userId = "25c705c0-76c8-4296-89d2-2128deb96280";
+            //var courses = await _dbReadService.GetAsync<UserCourse>(uc => uc.UserId.Equals(userId), true);
+            //courses.Select(uc => uc.Course).ToList();
+
+           
 
             if (!_signInManager.IsSignedIn(User))
                 return RedirectToPage("/Account/Login", new { Area = "Identity" });
