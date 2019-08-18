32c35582a4822f890b0333b4f964f2b0ab183b21
Alexander Akimov <akimov.infotech@gmail.com>
2019-08-18 20:34:08 +0600

    configure dependencies

--- VOD.API/Startup.cs
+++ VOD.API/Startup.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using VOD.Common.AutoMapper;
+using VOD.Common.Services;
+using VOD.Database;
+using VOD.Database.Services;
+using VOD.Domain.Entities;
 
 namespace VOD.API
 {
@@ -25,7 +34,23 @@ namespace VOD.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VODContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<VODUser>()
+                .AddRoles<IdentityRole>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<VODContext>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAutoMapper(typeof(AdminMappingProfile));
+            services.AddScoped<IDbReadService, DbReadService>();
+            services.AddScoped<IDbWriteService, DbWriteService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAdminService, AdminEFService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
