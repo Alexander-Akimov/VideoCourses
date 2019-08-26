121baa23ac68b1f8ca9ea814b087463b86a52502
Alexander <a.akimov@dostek.kg>
2019-08-26 18:28:45 +0600

    Instructors Api Controller

--- VOD.API/Controllers/InstructorsController.cs
+++ VOD.API/Controllers/InstructorsController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using VOD.Common.Services;
+using VOD.Domain.DTOModles.Admin;
+using VOD.Domain.Entities;
 
 namespace VOD.API.Controllers
 {
@@ -11,6 +15,63 @@ namespace VOD.API.Controllers
     [ApiController]
     public class InstructorsController : ControllerBase
     {
+        private readonly IAdminService _adminService;
+        private readonly LinkGenerator _linkGenerator;
+        public InstructorsController(IAdminService adminService, LinkGenerator linkGenerator)
+        {
+            this._adminService = adminService;
+            this._linkGenerator = linkGenerator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<InstructorDTO>>> GetAsync(bool include = false)
+        {
+            try
+            {
+                return await _adminService.GetAsync<Instructor, InstructorDTO>();
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Filure");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<InstructorDTO>> GetSingleAsync(int id)
+        {
+            try
+            {
+                var dto = await _adminService.SingleAsync<Instructor, InstructorDTO>(instr => instr.Id.Equals(id));
+                if (dto == null) return NotFound();
+                return dto;
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Filure");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<InstructorDTO>>> PostAsync(InstructorDTO model)
+        {
+            try
+            {
+                if (model == null) return BadRequest("No entity provided");
+
+                var id = await _adminService.CreateAsync<InstructorDTO, Instructor>(model);
+                var dto = await _adminService.SingleAsync<Instructor, InstructorDTO>(instr => instr.Id.Equals(id));
+                if (dto == null) return BadRequest("Unable to add entity");
+
+                var uri = _linkGenerator.GetPathByAction("GetSingleAsync", "Instructors", new { id });
+
+                return Created(uri, dto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to add the entity");
+            }
+        }
 
     }
 }
--- VOD.API/Startup.cs
+++ VOD.API/Startup.cs
@@ -66,7 +66,7 @@ namespace VOD.API
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
