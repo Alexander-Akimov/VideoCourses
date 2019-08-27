396a950d3587ed1fceb1ceda078954cd03dc1571
Alexander <a.akimov@dostek.kg>
2019-08-27 14:14:40 +0600

    put, delete

--- VOD.API/Controllers/InstructorsController.cs
+++ VOD.API/Controllers/InstructorsController.cs
@@ -22,8 +22,9 @@ namespace VOD.API.Controllers
             this._adminService = adminService;
             this._linkGenerator = linkGenerator;
         }
+
         [HttpGet]
-        public async Task<ActionResult<List<InstructorDTO>>> GetAsync(bool include = false)
+        public async Task<ActionResult<List<InstructorDTO>>> Get(bool include = false)
         {
             try
             {
@@ -31,20 +32,18 @@ namespace VOD.API.Controllers
             }
             catch (Exception)
             {
-
                 return StatusCode(StatusCodes.Status500InternalServerError, "Database Filure");
             }
         }
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<InstructorDTO>> GetSingleAsync(int id)
+        public async Task<ActionResult<InstructorDTO>> Get(int id)
         {
             try
             {
                 var dto = await _adminService.SingleAsync<Instructor, InstructorDTO>(instr => instr.Id.Equals(id));
                 if (dto == null) return NotFound();
                 return dto;
-
             }
             catch (Exception)
             {
@@ -53,7 +52,7 @@ namespace VOD.API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<List<InstructorDTO>>> PostAsync(InstructorDTO model)
+        public async Task<ActionResult<List<InstructorDTO>>> Post(InstructorDTO model)
         {
             try
             {
@@ -63,7 +62,7 @@ namespace VOD.API.Controllers
                 var dto = await _adminService.SingleAsync<Instructor, InstructorDTO>(instr => instr.Id.Equals(id));
                 if (dto == null) return BadRequest("Unable to add entity");
 
-                var uri = _linkGenerator.GetPathByAction("GetSingleAsync", "Instructors", new { id });
+                var uri = _linkGenerator.GetPathByAction("Get", "Instructors", new { id });
 
                 return Created(uri, dto);
             }
@@ -71,6 +70,45 @@ namespace VOD.API.Controllers
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Failed to add the entity");
             }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id, InstructorDTO model)
+        {
+            try
+            {
+                if (model == null) return BadRequest("No entity provided");
+                if (!id.Equals(model.Id)) return BadRequest("Differing ids");
+
+                var exist = await _adminService.AnyAsync<Instructor>(instr => instr.Id.Equals(id));
+                if (!exist) return NotFound("Could not find entity");
+
+                if (await _adminService.UpdateAsync<InstructorDTO, Instructor>(model))
+                    return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to update the entity");
+            }
+            return BadRequest("Unable to update the entity");
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var exist = await _adminService.AnyAsync<Instructor>(instr => instr.Id.Equals(id));
+                if (!exist) return BadRequest("Could not find entity");
+
+                if (await _adminService.DeleteAsync<Instructor>(d => d.Id.Equals(id)))
+                    return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to delete the entity");
+            }
+            return BadRequest("Failed to delete the entity");
         }
 
     }
